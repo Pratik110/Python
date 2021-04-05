7360413d81b85ae5ffb68cc01832c319bd9772d9
Pratik Patra <pratikpatra09@gmail.com>
2021-04-05 17:57:58 +0530

    Code Complete

--- Algorithms/Sieve_of_Eratosthenes.py
+++ Algorithms/Sieve_of_Eratosthenes.py
@@ -1,1 +1,23 @@
 Link = "https://www.geeksforgeeks.org/python-program-for-sieve-of-eratosthenes/"
+Description = "I am using this to find all the prime number in a given range.We'll create a boolean array of range " \
+              "up to n+1 with all the values initialized with True Where we find any number as a multiple of any " \
+              "other number, we'll assign it as False. So at the end we'll be left with an array consisting True at " \
+              "the position of prime numbers"
+def SieveOfEratosthenes(n):
+    arr = [True] * (n+1)
+    arr[0] = arr[1] = False
+    i = 2
+    while i*i < n:
+        for j in range(2*i,len(arr)):
+            if j%i == 0:
+                arr[j] = False
+        i+=1
+    return arr
+if __name__ == '__main__':
+    n = 30
+    arr = SieveOfEratosthenes(n)
+    for i in range(1,len(arr)):
+        mark = "Not Prime"
+        if arr[i]:
+            mark = "Prime"
+        print(i , " is ",mark)
