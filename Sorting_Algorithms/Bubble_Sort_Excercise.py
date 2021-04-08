edefc2e2430e0bf858fb59983b6702b931d07963
Pratik Patra <pratikpatra09@gmail.com>
2021-04-08 07:53:49 +0530

    Code Complete

---
+++ Sorting_Algorithms/Bubble_Sort_Excercise.py
@@ -0,0 +1,11 @@
+Link = "https://github.com/codebasics/data-structures-algorithms-python/blob/master/algorithms/2_BubbleSort/bubble_sort_exercise.md"
+def bubble_sort(elements):
+    for i in range
+
+bubble_sort(elements)
+elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
