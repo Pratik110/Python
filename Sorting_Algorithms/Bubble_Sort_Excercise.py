b2958524370dbb9c0b71a938515e2331198ee657
Pratik Patra <pratikpatra09@gmail.com>
2021-04-09 22:01:42 +0530

    Code Complete

--- Sorting_Algorithms/Bubble_Sort_Excercise.py
+++ Sorting_Algorithms/Bubble_Sort_Excercise.py
@@ -1,11 +1,24 @@
 Link = "https://github.com/codebasics/data-structures-algorithms-python/blob/master/algorithms/2_BubbleSort/bubble_sort_exercise.md"
-def bubble_sort(elements):
-    for i in range
+def bubble_sort(elements,key):
+    l = len(elements)
+    for i in range(l-1):
+        swapped = False
+        for j in range(l-1):
+            if elements[j][key] > elements[j+1][key]:
+                temp = elements[j]
+                elements[j] = elements[j+1]
+                elements[j+1] = temp
+                swapped = True
+        if not swapped:
+            break
+    return  elements
 
-bubble_sort(elements)
+
 elements = [
         { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
         { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
         { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
         { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
     ]
+key = 'name'
+print(bubble_sort(elements,key))
