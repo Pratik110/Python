ba37327a2c739221575cba077da6d9a5bad7bfac
Pratik Patra <pratikpatra09@gmail.com>
2021-04-11 09:51:11 +0530

    Code Complete

--- Array/Merge_Sorted_Arrays.py
+++ Array/Merge_Sorted_Arrays.py
@@ -1,4 +1,5 @@
-Link = "https://www.geeksforgeeks.org/efficiently-merging-two-sorted-arrays-with-o1-extra-space/"
+Link1 = "https://www.geeksforgeeks.org/efficiently-merging-two-sorted-arrays-with-o1-extra-space/"
+Link2 = "https://www.techiedelight.com/inplace-merge-two-sorted-arrays/"
 Description = "Given two sorted arrays, we need to merge them in O((n+m)*log(n+m)) time with O(1) extra " \
               "space into a sorted array, when n is the size of the first array, and m is the size of the" \
               "second array."
@@ -36,4 +37,30 @@ class Solution2:
 print(*Solution2().mergeSortedArray(ar1,ar2))   # * collects/unpacks all the positional arguments/ elements in a tuple.
                                                 # ** collects/unpacks all the keyword arguments/ elements in a dictionary.
 
-Link = "https://www.techiedelight.com/inplace-merge-two-sorted-arrays/"
+# Approach 3, Gap Algorithm
+ar1 = [1, 5, 9, 10, 15, 20]
+ar2 = [2, 3, 8, 13]
+import math
+class Solution3:
+    def mergeSortedArray(self,ar1,ar2):
+        l1 = len(ar1)
+        l2 = len(ar2)
+        ar1+=ar2
+        l = len(ar1)
+        gap = math.ceil(l/2)
+        while gap>0:
+            i = 0
+            j = i+gap
+            while j < l:
+                if ar1[i] > ar1[j]:
+                    temp = ar1[i]
+                    ar1[i] = ar1[j]
+                    ar1[j] = temp
+                i+=1
+                j+=1
+            gap//=2
+        whole_array = ar1
+        ar1 = whole_array[:l1]
+        ar2 = whole_array[l1:]
+        return ar1, ar2
+print(*Solution3().mergeSortedArray(ar1,ar2))
