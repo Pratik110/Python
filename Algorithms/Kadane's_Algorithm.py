4d095d971513ddb52852b270a8b808eea20cbc95
Pratik Patra <pratikpatra09@gmail.com>
2021-04-11 23:06:40 +0530

    Code Complete

--- Algorithms/Kadane's_Algorithm.py
+++ Algorithms/Kadane's_Algorithm.py
@@ -11,45 +11,8 @@ Example_2 = "Input: nums = [5,4,-1,7,8]'" \
             "Output: 23"
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]
-# Approach 1
-# Brute Force solution in BigO(n^3)
-class Solution1:
-    def maxSubArray(self, nums):
-        maxm = 0
-        l = len(nums)
-        for i in range(l):
-            for j in range(i,l):
-                subArraySum = self.sum(nums[i:j+1])
-                if subArraySum > maxm:
-                    maxm = subArraySum
-        return maxm
-
-    def sum(self,array):
-        s = 0
-        for i in array:
-            s+=i
-        return s
-print(Solution1().maxSubArray(nums))
-
-# Approach 2
-# Optimization of the Brute Force solution by reducing
-# the time complexity to BigO(n^2) from BigO(n^3)
-class Solution2:
-    def maxSubArray(self,nums):
-        maxm = 0
-        l = len(nums)
-        for i in range(l):
-            s = 0
-            for j in range(i,l):
-                s+=nums[j]
-                if s>maxm:
-                    maxm = s
-        return maxm
-print(Solution2().maxSubArray(nums))
-
-# Approach 3
 # Solving the problem using Kadane's Algorithm which reduces the time complexity to BigO(n)
-class Solution3:
+class Solution:
     def maxSubArray(self,nums):
         currSum = 0
         maxSum = nums[0]
@@ -61,4 +24,4 @@ class Solution3:
             if currSum < 0:
                 currSum = 0
         return  maxSum
-print(Solution3().maxSubArray(nums))
+print(Solution().maxSubArray(nums))
